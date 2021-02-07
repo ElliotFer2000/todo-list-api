0faa2acf9c0a934566cadd8d870e493cdd496628
Elliot Williamson <elliotferminwilliamsonsandoval@gmail.com>
2021-02-06 18:51:52 -0600

    Basic project structure was created.
    The unique files with content currently are in the models folder
    in the "task-management" module, located in the "modules/" folder.
    
    The models of the task-management module are finished.

---
+++ src/modules/task-management/business/models/task-list.js
@@ -0,0 +1,6 @@
+function TaskList({name,creationDate}){
+    this.name = name
+    this.creationDate = creationDate
+}
+
+module.exports.TaskList = TaskList
---
+++ src/modules/task-management/business/models/task.js
@@ -0,0 +1,6 @@
+function Task({title,state}){
+    this.title = title
+    this.state = state
+}
+
+module.exports.Task = Task
