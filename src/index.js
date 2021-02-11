b364dd05a3d4fbe9c8966d481081d99bf945fe55
Elliot Williamson <elliotferminwilliamsonsandoval@gmail.com>
2021-02-10 19:14:36 -0600

    imports of the module were modified in the global index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 const bodyParser = require("body-parser")
 const expressApp = express()
-const taskManagementRouting  = require("./modules/task-management/endpoints/router")
-const usersRouting = require("./modules/auth/endpoints/router")
-const taskManagementRepository = require("./modules/task-management/repository/todo-repository")
-const usersRepository = require("./modules/auth/repository/users-repository")
+const {authRepository,authRouting} = require("./modules/auth/endpoints/router")
+const {todoRepository,todoRouting} = require("./modules/task-management/index")
 const {makeDb} = require("./db/index")
 
 require("dotenv").config({path: `${__dirname}/../.env`})
@@ -14,8 +12,8 @@ async function startUpApp(){
     const db = await makeDb()
 
     expressApp.use(bodyParser.json())
-    expressApp.use("/task-management/",taskManagementRouting(taskManagementRepository(db)))
-    expressApp.use("/auth/",usersRouting(usersRepository(db)))
+    expressApp.use("/task-management/",todoRouting(todoRepository(db)))
+    expressApp.use("/auth/",authRouting(authRepository(db)))
     expressApp.listen(port,'localhost',()=>{
         console.log(`Server listening on port ${port}`)
     })
